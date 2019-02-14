2efc226843b2543140e848a8bf5e5b784ecfb18f
Aldmar <aldmar.joubert@students.plymouth.ac.uk>
2019-02-14 12:21:40 +0000

    Preparing minimal viable product

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -2,14 +2,17 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { Link, StaticQuery, graphql } from 'gatsby'
-import Img from 'gatsby-image'
+//import Img from 'gatsby-image'
 
 import { Navigation } from '.'
-import config from '../../utils/siteConfig'
+//import config from '../../utils/siteConfig'
 
 // Styles
 import '../../styles/app.css'
 import '../../styles/home-header.css'
+
+// Images
+//import Anna from '../../images/anna2.png'
 
 import Particle from '../design/ParticleComponent'
 
@@ -23,8 +26,8 @@ import Particle from '../design/ParticleComponent'
 */
 const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
     const site = data.allGhostSettings.edges[0].node
-    const twitterUrl = site.twitter ? `https://twitter.com/${site.twitter.replace(/^@/, ``)}` : null
-    const facebookUrl = site.facebook ? `https://www.facebook.com/${site.facebook.replace(/^\//, ``)}` : null
+    // const twitterUrl = site.twitter ? `https://twitter.com/${site.twitter.replace(/^@/, ``)}` : null
+    // const facebookUrl = site.facebook ? `https://www.facebook.com/${site.facebook.replace(/^\//, ``)}` : null
 
     return (
     <>
@@ -41,21 +44,22 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
                     <div className="stars">
                         <Particle/>
                     </div>
+                    
                     <div className="container">
                         <div className="site-mast">
-                            <div className="site-mast-left">
+                            {/* <div className="site-mast-left">
                                 <Link to="/">
                                     {site.logo ?
                                         <img className="site-logo" src={site.logo} alt={site.title} />
                                         : <Img fixed={data.file.childImageSharp.fixed} alt={site.title} />
                                     }
                                 </Link>
-                            </div>
-                            <div className="site-mast-right">
+                            </div> */}
+                            {/* <div className="site-mast-right">
                                 { site.twitter && <a href={ twitterUrl } className="site-nav-item" target="_blank" rel="noopener noreferrer"><img className="site-nav-icon" src="/images/icons/twitter.svg" alt="Twitter" /></a>}
                                 { site.facebook && <a href={ facebookUrl } className="site-nav-item" target="_blank" rel="noopener noreferrer"><img className="site-nav-icon" src="/images/icons/facebook.svg" alt="Facebook" /></a>}
                                 <a className="site-nav-item" href={ `https://feedly.com/i/subscription/feed/${config.siteUrl}/rss/` } target="_blank" rel="noopener noreferrer"><img className="site-nav-icon" src="/images/icons/rss.svg" alt="RSS Feed" /></a>
-                            </div>
+                            </div> */}
                         </div>
                         { isHome ?
                             <div className="">
@@ -64,16 +68,16 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
                                     <p className="site-banner-desc">{site.description}</p>
                                 </div> 
                             </div> :
-                            null}
-                        <nav className="site-nav">
-                            <div className="site-nav-left">
-                                {/* The navigation items as setup in Ghost */}
-                                <Navigation data={site.navigation} navClass="site-nav-item" />
-                            </div>
-                            <div className="site-nav-right">
-                                <Link className="site-nav-button" to="/about">About</Link>
-                            </div>
-                        </nav>
+                            <nav className="site-nav">
+                                <div className="site-nav-right">
+                                    {/* The navigation items as setup in Ghost */}
+                                    <Navigation data={site.navigation} navClass="site-nav-item" />
+                                </div>
+                                {/* <div className="site-nav-right">
+                                    <Link className="site-nav-button" to="/about">About</Link>
+                                </div> */}
+                            </nav>}
+                        
                     </div>
                 </header>
 
@@ -89,7 +93,7 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
                 <footer className="site-foot">
                     <div className="site-foot-nav container">
                         <div className="site-foot-nav-left">
-                            <Link to="/">{site.title}</Link> © 2019 &mdash; Published with <a className="site-foot-nav-item" href="https://ghost.org" target="_blank" rel="noopener noreferrer">Ghost</a>
+                            <Link to="/">{site.title}</Link> © 2019 &mdash; Built by Aldmar Joubert
                         </div>
                         <div className="site-foot-nav-right">
                             <Navigation data={site.navigation} navClass="site-foot-nav-item" />
